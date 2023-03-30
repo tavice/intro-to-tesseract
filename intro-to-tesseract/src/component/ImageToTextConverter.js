b7a25dd8421d3aff9b15d42921c0b647dda10021
tavice <thomasavice.ta@gmail.comm>
2023-03-29 17:34:10 -0700

    updated css

--- intro-to-tesseract/src/component/ImageToTextConverter.js
+++ intro-to-tesseract/src/component/ImageToTextConverter.js
@@ -5,15 +5,19 @@ import preprocessImage from "./preprocess";
 function ImageToTextConverter() {
   const [imagePath, setImagePath] = useState("");
   const [text, setText] = useState("");
+  const [confidence, setConfidence] = useState("");
   const canvasRef = useRef(null);
   const imageRef = useRef(null);
 
   const handleChange = (event) => {
     setImagePath(URL.createObjectURL(event.target.files[0]));
+    console.log((event.target.files[0]))
   };
 
-  const handleClick = () => {
-    const canvas = canvasRef.current;
+
+
+  const handleClick = () => { 
+    const canvas = canvasRef.current; 
     const ctx = canvas.getContext("2d");
 
     console.log(imageRef.current.complete);
@@ -32,6 +36,7 @@ function ImageToTextConverter() {
         .then((result) => {
           let confidence = result.data.confidence;
             console.log(confidence);
+            setConfidence(confidence);
           let text = result.data.text;
           setText(text);
         });
@@ -43,7 +48,7 @@ function ImageToTextConverter() {
   return (
     <div className="App">
       <main className="App-main">
-        <h3>Actual imagePath uploaded</h3>
+        <h3 className="imagePath">Actual imagePath uploaded</h3>
         <img
           src={imagePath}
           className="App-image"
@@ -52,17 +57,23 @@ function ImageToTextConverter() {
           onLoad={() => console.log("Image loaded!")}
         />
 
-        <h3>Canvas</h3>
-        <canvas ref={canvasRef} width={700} height={250}></canvas>
+        <h3 className="canvas">Canvas</h3>
+        <canvas ref={canvasRef} width={500} height={500}></canvas>
         <h3>Extracted text</h3>
         <div className="pin-box">
           <p> {text} </p>
         </div>
+        <div className="button-box">
         <input type="file" onChange={handleChange} />
         <button onClick={handleClick} style={{ height: 50 }}>
           {" "}
           convert to text
         </button>
+        </div>
+        <h3 className="confidence">Confidence</h3>
+        <div className="pin-box">
+            <p> {confidence} %</p>
+        </div>
       </main>
     </div>
   );
